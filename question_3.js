a20bc56d70a5d44fbdf7bff39ce74f77ebc3642a
kishore-888 <kishorekcu57075@gmail.com>
2024-01-26 16:45:00 +0530

    Updated all files

---
+++ question_3.js
@@ -0,0 +1,39 @@
+function openModal1(imageSrc) {
+    var popup1 = document.getElementById("popup1");
+    var popupImg1 = document.getElementById("popupImage1");
+    popup1.style.display = "block";
+    popupImg1.src = imageSrc;
+}
+
+function closeModal1() {
+    var popup1 = document.getElementById("popup1");
+    popup1.style.display = "none";
+
+}
+
+function openModal2(imageSrc) {
+    var popup2 = document.getElementById("popup2");
+    var popupImg2 = document.getElementById("popupImage2");
+    popup2.style.display = "block";
+    popupImg2.src = imageSrc;
+}
+
+function closeModal2() {
+    var popup2 = document.getElementById("popup2");
+    popup2.style.display = "none";
+
+}
+
+function openModal3(imageSrc) {
+    var popup3 = document.getElementById("popup3");
+    var popupImg3 = document.getElementById("popupImage3");
+    popup3.style.display = "block";
+    popupImg3.src = imageSrc;
+}
+
+function closeModal3() {
+    var popup3 = document.getElementById("popup3");
+    popup3.style.display = "none";
+
+}
+
